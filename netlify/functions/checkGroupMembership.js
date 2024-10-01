3a4fc0c43bc9ea223673de8cd97a7c5f71e10b10
makscatt <makscatt@gmail.com>
2024-10-01 10:36:32 +0300

    Обновление

--- netlify/functions/checkGroupMembership.js
+++ netlify/functions/checkGroupMembership.js
@@ -1,12 +1,29 @@
-const axios = require('axios');
-
 exports.handler = async function(event, context) {
     const BOT_TOKEN = '6974834378:AAEckz1HNBH9RiKvz-uqM2Upcdi7KKjAwe8'; // Вставьте ваш токен бота
-    const CHAT_ID = 'ВАШ_CHAT_ID'; // Вставьте ID группы/чата
-    const userId = JSON.parse(event.body).userId;
+    const CHAT_ID = '-1002123915695'; // Вставьте ID группы/чата
+
+    let userId;
+    
+    // Проверка на наличие тела запроса
+    try {
+        const body = event.body ? JSON.parse(event.body) : {};
+        userId = body.userId;
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Invalid request body' })
+        };
+    }
+
+    if (!userId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'User ID not provided' })
+        };
+    }
 
     try {
-        // Отправляем запрос к Telegram API, чтобы проверить, является ли пользователь членом группы
+        // Ваш запрос к Telegram API для проверки члена группы
         const response = await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/getChatMember`, {
             params: {
                 chat_id: CHAT_ID,
@@ -16,7 +33,6 @@ exports.handler = async function(event, context) {
 
         const chatMember = response.data.result;
 
-        // Проверяем статус пользователя
         if (chatMember && ['member', 'administrator', 'creator'].includes(chatMember.status)) {
             return {
                 statusCode: 200,
@@ -33,7 +49,7 @@ exports.handler = async function(event, context) {
         console.error('Ошибка при проверке пользователя:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ isMember: false, error: 'Ошибка на сервере' })
+            body: JSON.stringify({ error: 'Ошибка при проверке пользователя' })
         };
     }
 };
