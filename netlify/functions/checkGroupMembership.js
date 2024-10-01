f8a520e17840e98cc4232c7d769683285543a67c
makscatt <makscatt@gmail.com>
2024-10-01 13:01:17 +0300

    Обновление

--- netlify/functions/checkGroupMembership.js
+++ netlify/functions/checkGroupMembership.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
-    const BOT_TOKEN = '6974834378:AAEckz1HNBH9RiKvz-uqM2Upcdi7KKjAwe8'; // Вставьте ваш токен бота
-    const CHAT_ID = '-1002123915695'; // Вставьте ID группы/чата
+    const BOT_TOKEN = '6974834378:AAEckz1HNBH9RiKvz-uqM2Upcdi7KKjAwe8'; // Твой токен бота
+    const CHAT_ID = '-1002123915695'; // Твой ID группы
 
     let userId;
 
@@ -13,6 +13,10 @@ exports.handler = async function(event, context) {
     } catch (error) {
         return {
             statusCode: 400,
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Разрешаем запросы с любых доменов
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({ error: 'Invalid request body' })
         };
     }
@@ -20,6 +24,10 @@ exports.handler = async function(event, context) {
     if (!userId) {
         return {
             statusCode: 400,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({ error: 'User ID not provided' })
         };
     }
@@ -35,22 +43,22 @@ exports.handler = async function(event, context) {
 
         const chatMember = response.data.result;
 
-        if (chatMember && ['member', 'administrator', 'creator'].includes(chatMember.status)) {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ isMember: true })
-            };
-        } else {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ isMember: false })
-            };
-        }
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*', // CORS
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ isMember: chatMember && ['member', 'administrator', 'creator'].includes(chatMember.status) })
+        };
 
     } catch (error) {
-        console.error('Ошибка при проверке пользователя:', error);
         return {
             statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({ error: 'Ошибка при проверке пользователя' })
         };
     }
